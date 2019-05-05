5588afcede486c23cb8dda321a530e55738a6d84
rpigott <rpigott@berkeley.edu>
2019-05-05 12:20:52 -0700

    refactor

--- mcidle.py
+++ mcidle.py
@@ -1,76 +1,82 @@
 import socket, socketserver
-import struct, json, io
-from operator import or_
+import struct, json, io, copy
+import argparse
 
-SERVER_INFO = {
-	'version': {
-		'name': '1.14',
-	},
-	'players': {
-		'max': 0,
-		'online': 0
-	},
-	'description': {
-		'text': 'GCP idle server'
-	}
-}
+parser = argparse.ArgumentParser(
+	description = "Shell server for handling minecraft SLP requests",
+	formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 
-def pack_varint(n):
-	parts = [(n >> (7 * k)) & 0x7f for k in range((n.bit_length() - 1) // 7 + 1)]
-	bits = [0x80] * (len(parts) - 1) + [0x00]
-	res = b''
-	for part, bit in zip(parts, bits):
-		res += struct.pack('B', bit | part)
-	return res
+parser.add_argument('--description', help = "server description text")
+parser.add_argument('--host', default = "0.0.0.0",
+	help = "address to listen on. Minecraft does not support IPv6.")
+parser.add_argument('--port',
+	help = "port to listen on.",
+	default = 25565, type = int)
 
-def unpack_varint(b):
-	return sum((n & 0x7f) << (7 * k) for k, n in enumerate(b))
+args = parser.parse_args()
 
-def pack_string(s):
-	return pack_varint(len(s)) + s.encode('utf-8')
+class MinecraftServer(socketserver.TCPServer):
+	def __init__(self, server_info, *args, **kwds):
+		super().__init__(*args, **kwds)
+		self.server_info = server_info
+
+class MCFormatError(Exception):
+	"""Minecraft protocol format error"""
+	pass
 
 class MCVarInt:
-	def __init__(self, value):
+	"""
+	VarInt type from the Minecraft protocol.
+	7 LSB are data bits. MSB of 0 indicates this byte is the last.
+	"""
+	def __init__(self, value: int):
 		self.value = value
 	
-	def pack(self):
-		if not self.value:
-			return b"\x00"
-	
-		parts = [(self.value >> (7 * k)) & 0x7f for k in range((self.value.bit_length() - 1) // 7 + 1)]
-		bits = [0x80] * (len(parts) - 1) + [0x00]
-		bs = b''
-		for part, bit in zip(parts, bits):
-			bs += struct.pack('B', bit | part)
-		return bs
-
-	@staticmethod
-	def unpack(bs):
-		return MCVarInt(sum((n & 0x7f) << (7 * k) for k, n in enumerate(bs)))
-	
-	@staticmethod
-	def read(rfile):
-		bs = rfile.read(1)
-		if not bs:
-			return None
-		while bs[-1] & 0x80:
-			bs += rfile.read(1)
-		return MCVarInt.unpack(bs)
-
 	def __len__(self):
-		return len(self.pack())
+		return (self.value.bit_length() - 1) // 7 + 1 if self.value else 1
 
 	def __repr__(self):
 		return f"MCVarInt({self.value})"
 
+	def pack(self):
+		if not self.value: return b"\x00"
+
+		parts = [(self.value >> (7 * k)) & 0x7f for k in range(len(self))]
+		return bytes([byte | 0x80 for byte in parts[:-1]]) + bytes(parts[-1:])
+
+	@staticmethod
+	def unpack(bs):
+		if not bs or bs[-1] & 0x80:
+			raise ValueError(f"Expected MCVarInt, not {bs!r}")
+
+		byte = lambda n, k: (n & 0x7f) << (7 * k)
+		return MCVarInt(sum(byte(n, k) for k, n in enumerate(bs)))
+	
+	@staticmethod
+	def read(rfile):
+		bs = rfile.read(1)
+		while bs and bs[-1] & 0x80:
+			bs += rfile.read(1)
+		return MCVarInt.unpack(bs)
+
 class MCString:
+	"""
+	String type from the Minecraft protocol.
+	string length as a VarInt followed by the data.
+	"""
 	def __init__(self, data):
 		if isinstance(data, str):
 			self.data = data.encode('utf-8')
 		elif isinstance(data, bytes):
 			self.data = data
 		else:
-			raise TypeError
+			raise ValueError
+
+	def __len__(self):
+		return len(MCVarInt(len(self.data))) + len(self.data)
+
+	def __repr__(self):
+		return f"MCString({self.data!r})"
 	
 	def pack(self):
 		return MCVarInt(len(self.data)).pack() + self.data
@@ -80,19 +86,17 @@ class MCString:
 		size = MCVarInt.read(rfile)
 		return MCString(rfile.read(size.value))
 
-	def __repr__(self):
-		return f"MCString({self.data!r})"
-
 class MinecraftRequestHandler(socketserver.StreamRequestHandler):
 	def mc_recv(self):
 		"""Return one minecraft protocol packet"""
-		size = MCVarInt.read(self.rfile)
-		if not size:
-			return None, 0
-		pid = MCVarInt.read(self.rfile)
-		data = self.rfile.read(size.value - len(pid))
-		buf = io.BytesIO(data)
-		return buf, pid.value
+		try:
+			size = MCVarInt.read(self.rfile)
+			pid = MCVarInt.read(self.rfile)
+			data = self.rfile.read(size.value - len(pid))
+			buf = io.BytesIO(data)
+			return buf, pid.value
+		except ValueError as error:
+			raise MCFormatError from error
 
 	def mc_send(self, payload, pid = 0):
 		"""Send one minecraft protocol packet"""
@@ -105,19 +109,18 @@ class SLPHandler(MinecraftRequestHandler):
 	def handle(self):
 		# Read protocol things
 		packet, pid = self.mc_recv() # Hello
-		if not packet:
-			return
 		req, _ = self.mc_recv() # empty request
 
 		# Read the client hello
+		server_info = copy.deepcopy(self.server.server_info)
 		protocol = MCVarInt.read(packet)
-		SERVER_INFO['version']['protocol'] = protocol.value
+		server_info['version']['protocol'] = protocol.value
 		addr = MCString.read(packet)
 		port, = struct.unpack('>h', packet.read(2))
 		state = MCVarInt.read(packet)
 
 		# Always return our status, don't care about other requests
-		status = MCString(json.dumps(SERVER_INFO))
+		status = MCString(json.dumps(server_info))
 		self.mc_send(status.pack())
 
 		# Client expects an echo
@@ -125,6 +128,22 @@ class SLPHandler(MinecraftRequestHandler):
 		self.mc_send(packet.read(), pid = pid)
 
 if __name__ == '__main__':
-	target = '0.0.0.0', 25565
-	with socketserver.TCPServer(target, SLPHandler) as server:
+	server_info = {
+		'version': {
+			'name': '1.14',
+		},
+		'players': {
+			'max': 0,
+			'online': 0
+		},
+		'description': {
+			'text': 'SLP idle server'
+		}
+	}
+
+	target = args.host, args.port
+	if args.description is not None:
+		server_info['description']['text'] = args.description
+
+	with MinecraftServer(server_info, target, SLPHandler) as server:
 		server.serve_forever()
